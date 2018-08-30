6ff5db6c15898cdf8ef8bb6eea789e96a3b02924
allstay <kim@allstayui-MacBook-Pro.local>
2018-08-30 13:57:55 +0900

    apply react-hot-loader

--- react-express-hot/config/webpack.config.dev.js
+++ react-express-hot/config/webpack.config.dev.js
@@ -33,6 +33,8 @@ module.exports = {
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
   entry: [
+    // react-hot-loader 적용하기
+    'react-hot-loader/patch',
     // We ship a few polyfills by default:
     require.resolve('./polyfills'),
     // Include an alternative client for WebpackDevServer. A client's job is to
@@ -155,6 +157,11 @@ module.exports = {
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
               cacheDirectory: true,
+
+              // react-hot-loader 적용하기
+              plugins: [
+                'react-hot-loader/babel'
+              ]
             },
           },
           // "postcss" loader applies autoprefixer to our CSS.
