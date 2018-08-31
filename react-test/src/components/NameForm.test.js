734714b9eb72be0ba92b00c1b3c28db71d4c1ad5
allstay <kim@allstayui-MacBook-Pro.local>
2018-08-31 17:46:30 +0900

    install enzyme and try DOM simulation
    - add setupTests.js for test setup which is requirement for CRA
    - apply enzyme

--- react-test/src/components/NameForm.test.js
+++ react-test/src/components/NameForm.test.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
 import NameForm from './NameForm';
 
 describe('NameForm', () => {
   let component = null;
 
   it('renders correctly', () => {
-    component = renderer.create(<NameForm />);
+    component = shallow(<NameForm />);
+    // component = renderer.create(<NameForm />);
   });
 
   it('matches snapshot', () => {
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(component).toMatchSnapshot();
+    // const tree = component.toJSON();
+    // expect(tree).toMatchSnapshot();
   });
+
+  // DOM 시뮬레이션
+  describe('insert new text', () => {
+    it('has a form', () => {
+      expect(component.find('form').exists()).toBe(true);
+    })
+    it('has an input', () => {
+      expect(component.find('input').exists()).toBe(true);
+    })
+  })
 });
